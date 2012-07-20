ed70bb9f191e8e3fb85250bf684db4956b962dca
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-07-20 10:46:11 -0500

    first draft of middleware

---
+++ django-plop/middleware.py
@@ -0,0 +1,37 @@
+'plop middleware'
+from django.conf import settings
+import plop
+import os
+
+try:
+    PLOP_DIR = settings.PLOP_DIR
+except AttributeError:
+    os.environ.get('PLOP_DIR', '/tmp/plop')
+
+
+class PlopMiddleware(object):
+    'middleware to profile views with plop'
+    def __init__(self):
+        'make sure PLOP_DIR exists'
+        if not os.path.exists(PLOP_DIR):
+            os.mkdir(PLOP_DIR)
+
+    def get_filename(self, func):
+        'get the filename for a function'
+        return os.path.join(PLOP_DIR, func.__name__)
+
+    def process_view(self, request, view_func, _, __):
+        'process a single view, adding the collector'
+        request.collector = plop.collector.Collector()
+        request.collector.start()
+        request.plop_filename = self.get_filename(view_func)
+
+    def process_response(self, request, response):
+        'after the view executes, stop profiling'
+        request.collector.stop()
+        with open(request.plop_filename, 'a') as plop_file:
+            plop_file.write(repr(dict(plop.stack_counts)))
+
+    def process_exception(self, request, exception):
+        'process an exception - just stop profiling'
+        request.collector.stop()
